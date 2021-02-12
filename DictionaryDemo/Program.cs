c29784e08dc662c56b04a8bf8f41855ece7b889d
Samet Vural <vuralasamet@outlook.com>
2021-02-12 22:40:59 +0300

    Add project files.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Sezer");
+            myDictionary.Add(2, "Celal");
+            myDictionary.Add(3, "Bekir");
+            myDictionary.Add(3, "Metin");
+
+            myDictionary.ToList();
+        }
+    }
+}
